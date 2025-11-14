c35a4ad4a6ce3585f6209772903e5be5c56b83bc
Danielly Aiko Messias <daniellyaiko2021@gmail.com>
2025-11-14 17:07:47 -0300

    vetor_com_marca_de_carros.cpp

---
+++ vetor_com_marca_de_carros.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ #include <string>
+using namespace std;
+
+int main()
+{
+  string carro[7] = {"bmw","toyota","mercedes","ford","nissan","ferrari","audi"};
+   
+   cout << carro[0] << endl;
+    cout << carro[1] << endl;
+     cout << carro[2] << endl;
+      cout << carro[3] << endl;
+       cout << carro[4] << endl;
+        cout << carro[5] << endl;
+         cout << carro[6] << endl;
+    return 0;
+}
+
