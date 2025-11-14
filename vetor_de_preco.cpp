bf703ded41bc132fba030e1c06375008135fcdfe
Danielly Aiko Messias <daniellyaiko2021@gmail.com>
2025-11-14 17:06:23 -0300

    vetor_de_preco.cpp

---
+++ vetor_de_preco.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+ #include <string>
+using namespace std;
+
+int main()
+{
+  float preco[15] = {1.99,2.50,3.30,7.80,99.99,150.15,79.55,7.99,78.10,85.98,399.99,859.69,659.17,74.748,48.78};
+   
+   cout << preco[14];
+    return 0;
+}
