6165142974232563c770f7955434903225057461
Danielly Aiko Messias <daniellyaiko2021@gmail.com>
2025-11-14 17:05:06 -0300

    vetores_com_frutas.cpp

---
+++ vetores_com_frutas.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+ #include <string>
+using namespace std;
+
+int main()
+{
+  string frutas[15] = {"banana","maça","morango","caju","melão","melancia","tomate","pepino","alface","cenoura","brócolis","pitaya","graviola","acerola","kiwi"};
+   
+   cout << frutas[12];
+    return 0;
+}
+
